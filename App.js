5a682c6ca21c9313a4a1333985c179a7c388c000
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-02-18 21:41:39 -0300

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Start Up Nasa Project</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  }
 });
